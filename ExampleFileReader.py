c9995d971a114760377d4dc14f4841964972d7d7
Ghostkeeper <rubend@tutanota.com>
2017-09-29 15:45:14 +0200

    Implement the file reader
    This is luckily very simple.
    
    Made for internal ticket CURA-4347.

---
+++ ExampleFileReader.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2017 Ultimaker B.V.
+# This example is released under the terms of the AGPLv3 or higher.
+
+from UM.Mesh.MeshBuilder import MeshBuilder #To create a mesh to put in the scene.
+from UM.Mesh.MeshReader import MeshReader #This is the plug-in object we need to implement if we want to create meshes. Otherwise extend from FileReader.
+from UM.Math.Vector import Vector #Helper class required for MeshBuilder.
+from UM.Scene.SceneNode import SceneNode #The result we must return when reading.
+
+class ExampleFileReader(MeshReader):
+    def __init__(self):
+        super().__init__()
+        self._supported_extensions = [".txt"] #Sorry, you also have to specify it here.
+
+    ##  Read the specified file.
+    #
+    #   \return A scene node that represents what's in the file.
+    def read(self, file_name):
+        #Here you would typically open the file, read its contents, parse it, etc.
+        #For this example we will just always create a cube and return that.
+
+        builder = MeshBuilder() #To construct your own mesh, look at the methods provided by MeshBuilder.
+        builder.addCube(10, 10, 10, Vector(0, 0, 0)) #Cube of 10 by 10 by 10.
+
+        #Put the mesh inside a scene node.
+        result_node = SceneNode()
+        result_node.setMeshData(builder.build())
+
+        return result_node
